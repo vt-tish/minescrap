fa2184fc346a96d4882269327a8c59fb7371c4e2
JaamalL <vitkt20@gmail.com>
2026-01-19 19:58:09 +0200

    chore(core): Clean up imports for EventRegister

--- src/main/java/com/vttish/minescrap/core/event/EventRegister.java
+++ src/main/java/com/vttish/minescrap/core/event/EventRegister.java
@@ -3,7 +3,6 @@ package com.vttish.minescrap.core.event;
 import com.vttish.minescrap.api.event.Listenable;
 import com.vttish.minescrap.api.event.Subscription;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
