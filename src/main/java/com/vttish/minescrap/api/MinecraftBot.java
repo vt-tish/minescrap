32b08bbc22022a05976f8d934e0a5794563f0730
JaamalL <vitkt20@gmail.com>
2026-01-22 13:12:21 +0200

    feat(api): Add main MinecraftBot interface

---
+++ src/main/java/com/vttish/minescrap/api/MinecraftBot.java
@@ -0,0 +1,21 @@
+package com.vttish.minescrap.api;
+
+import com.vttish.minescrap.api.entity.Player;
+import com.vttish.minescrap.api.event.Listenable;
+import com.vttish.minescrap.api.event.Subscription;
+
+public interface MinecraftBot {
+    void connect();
+    boolean isConnected();
+
+    void disconnect();
+    void disconnect(String reason);
+
+    void chat(String message);
+
+    String getUsername();
+    Player getPlayer();
+
+    <T extends Listenable> Subscription on(Class<T> event, T listener);
+    <T extends Listenable> Subscription once(Class<T> event, T listener);
+}
