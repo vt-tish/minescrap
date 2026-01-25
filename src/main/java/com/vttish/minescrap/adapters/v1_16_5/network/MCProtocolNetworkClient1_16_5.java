c695b55b8f7c76349b09ca1ca57c402222b256c8
JaamalL <vitkt20@gmail.com>
2026-01-25 16:01:31 +0200

    refactor(adapters): Make connect method implementation to recreate client every connection attempt

--- src/main/java/com/vttish/minescrap/adapters/v1_16_5/network/MCProtocolNetworkClient1_16_5.java
+++ src/main/java/com/vttish/minescrap/adapters/v1_16_5/network/MCProtocolNetworkClient1_16_5.java
@@ -10,17 +10,29 @@ import com.github.steveice10.packetlib.tcp.TcpSessionFactory;
 import com.vttish.minescrap.core.network.NetworkClient;
 
 public class MCProtocolNetworkClient1_16_5 implements NetworkClient {
-    private final Client client;
+    private Client client;
+    private PacketListener packetListener;
+    private final String username;
+    private final String host;
+    private final int port;
 
     public MCProtocolNetworkClient1_16_5(String username, String host, int port) {
-        MinecraftProtocol protocol = new MinecraftProtocol(username);
-
-        this.client = new Client(host, port, protocol, new TcpSessionFactory());
+        this.username = username;
+        this.host = host;
+        this.port = port;
+        createClient();
     }
 
     @Override
     public void connect() {
-        this.client.getSession().connect();
+        if (client != null && isConnected()) {
+            disconnect();
+        }
+
+        createClient();
+        setPacketListener(packetListener);
+
+        client.getSession().connect();
     }
 
     @Override
@@ -41,6 +53,12 @@ public class MCProtocolNetworkClient1_16_5 implements NetworkClient {
             throw new IllegalArgumentException("[MCProtocolNetworkClient]: Object should be an instance of Packet");
         }
 
+        if (!isConnected()) {
+            throw new IllegalArgumentException(
+                    "[MCProtocolNetworkClient]: Cannot send packet, client is not connected"
+            );
+        }
+
         client.getSession().send((Packet) packet);
     }
 
@@ -51,6 +69,7 @@ public class MCProtocolNetworkClient1_16_5 implements NetworkClient {
 
     @Override
     public void setPacketListener(PacketListener listener) {
+        this.packetListener = listener;
         client.getSession().addListener(new SessionAdapter() {
             @Override
             public void packetReceived(PacketReceivedEvent event) {
@@ -62,5 +81,10 @@ public class MCProtocolNetworkClient1_16_5 implements NetworkClient {
                 listener.onDisconnected(event.getReason());
             }
         });
+    }
+
+    private void createClient() {
+        MinecraftProtocol protocol = new MinecraftProtocol(username);
+        client = new Client(host, port, protocol, new TcpSessionFactory());
     }
 }
